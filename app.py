6be788a264f993e60cec6625bb4463dc3b300c7e
cat-the-coder <38890523+cat-the-coder@users.noreply.github.com>
2018-05-01 11:16:15 -0700

    added command
    added 2 commands

---
+++ app.py
@@ -0,0 +1,7 @@
+command = ""
+
+command = input("Insert Command: ")  if command == "Ping": print("pong")
+
+command = input("Insert Command: ")  if command == "apple": print("banana")
+
+command = input("Insert Command: ")  if command == "bread": print("peanuts")
