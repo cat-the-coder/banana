79d708598f413da46d963aa9b369001258476719
cat-the-coder <38890523+cat-the-coder@users.noreply.github.com>
2018-05-01 11:22:07 -0700

    fixed command
    fixed

---
+++ fixed.py
@@ -0,0 +1,16 @@
+command = ""
+
+
+command = input("Insert Command: ")
+if command == "Ping": 
+ print("pong")
+
+
+
+if command == "apple": 
+ print("banana")
+
+
+
+ if command == "bread": 
+ print("peanuts")
